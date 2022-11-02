fb1e22997ea53626758d67f216c41564f4aaf966
Mohammad Haziq Khan <117180322+haziq-ibm@users.noreply.github.com>
2022-11-02 08:00:34 -0400

    Add project files via upload
    uploaded whole project folder

---
+++ RoutingAPI/RoutingAPI/CommonMethods/Common.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutingAPI.CommonMethods
+{
+    public class Common
+    {
+        public static void ExecuteQuery(IConfiguration _configuration, List<string> qStrings, string tableName)
+        {
+            try
+            {
+                OleDbConnection conn = new OleDbConnection(_configuration.GetConnectionString(tableName));
+
+                conn.Open();
+                qStrings.ForEach(qStr => {
+                    OleDbCommand cmd = new OleDbCommand(qStr, conn);
+                    cmd.ExecuteNonQuery();
+                });
+                conn.Close();
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ RoutingAPI/RoutingAPI/Controllers/ImportController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace RoutingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<String>> CheckConn()
+        {
+            return "Connected to Routing API : Import Controller";
+        }
+
+        [HttpPost("zip")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            try
+            {
+                var folderName = Path.Combine("Resources", "uploads");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    string zipPath = Path.GetFileName(fullPath);
+                    ZipFile.ExtractToDirectory(fullPath, pathToSave);
+
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest("Error in uploading file");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+    }
+}
